b58b4054ec27e52c78b2f3c987c233b2377986d8
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-01-17 17:00:00 +0200

    ci: remove example component

--- src/components/Example/Example.stories.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Example } from './Example'
-
-storiesOf('Example', module)
-  .add('Default', () => <Example>Example</Example>);
