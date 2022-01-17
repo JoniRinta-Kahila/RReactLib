ae4a59631c57522b2a1b3a8587ac767ec5db29b3
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-01-17 16:56:35 +0200

    feat(collapsible): initial commit

---
+++ src/components/Collapsible/Collapsible.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Collapsible } from './Collapsible';
+
+storiesOf('collapsible', module)
+  .add('default', () => (
+    <Collapsible>
+    </Collapsible>
+  ))
+  .add('Example-1', () => (
+    <Collapsible
+      label='Example'
+      labelColor='"fff'
+      labelBackground='darkgrey'
+    >
+      <div>
+        <p>asd</p>
+        <p>kek</p>
+        <p>foo</p>
+        <p>bar</p>
+      </div>
+    </Collapsible>
+  ))
+  .add('Example-2', () => (
+    <>
+      <Collapsible
+        label='Collapsible-1'
+      >
+        <div>
+          <p>asd</p>
+          <p>kek</p>
+          <p>foo</p>
+          <p>bar</p>
+        </div>
+      </Collapsible>
+      <Collapsible
+        label='Collapsible-2'
+      >
+        <div>
+          <p>asd</p>
+          <p>kek</p>
+          <p>foo</p>
+          <p>bar</p>
+        </div>
+      </Collapsible>
+      <Collapsible
+        label='Collapsible-3'
+        defaultOpen
+      >
+        <div>
+          <p>asd</p>
+          <p>kek</p>
+          <p>foo</p>
+          <p>bar</p>
+        </div>
+      </Collapsible>
+    </>
+  ))
---
+++ src/components/Collapsible/Collapsible.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './collapsible.scss';
+
+export interface CollapsibleProps {
+  label?: string;
+  labelColor?: string;
+  labelBackground?: string;
+  containerClassName?: string;
+  containerStyle?: React.CSSProperties;
+  contentColor?: string;
+  contentBackground?: string;
+  defaultOpen?: boolean;
+}
+
+export const Collapsible: React.FC<CollapsibleProps> = (
+  {
+    children,
+    label,
+    labelColor,
+    labelBackground,
+    containerClassName,
+    containerStyle,
+    contentColor,
+    contentBackground,
+    defaultOpen = false,
+  }
+) => {
+  const [open, setOpen] = useState<boolean>(defaultOpen);
+  const btnRef: React.MutableRefObject<HTMLButtonElement|null> = useRef(null);
+  const contentRef: React.MutableRefObject<HTMLDivElement|null> = useRef(null);
+
+  useEffect(() => {
+    if (btnRef.current && contentRef.current) {
+      btnRef.current.classList.toggle('collapsible-active', open);
+      contentRef.current.style.maxHeight = open ? 'none' : '0px';
+    }
+  }, [open]);
+
+  return (
+    <div className={containerClassName} style={containerStyle}>
+      <div className='collapsible-container'>
+        <button
+          style={{
+            color: labelColor,
+            background: labelBackground
+          }}
+          ref={btnRef}
+          className='collapsible-toggle'
+          onClick={() => setOpen(!open)}
+          data-testid='input-element'
+        >
+          {
+            label
+          }
+        </button>
+        <div
+          className='collapsible-content'
+          ref={contentRef}
+          data-testid='div-element'
+          style={{
+            color: contentColor,
+            background: contentBackground,
+          }}
+        >
+          {
+            children
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
